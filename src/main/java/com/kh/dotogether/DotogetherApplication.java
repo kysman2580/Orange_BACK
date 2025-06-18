c898fd28a20b6a03ef7524cee6fd4c49e2664541
haruki975-gif <haruki975@gmail.com>
2025-06-18 15:41:08 +0900

    final-project back

---
+++ src/main/java/com/kh/dotogether/DotogetherApplication.java
@@ -0,0 +1,13 @@
+package com.kh.dotogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/dotogether/DotogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.dotogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
